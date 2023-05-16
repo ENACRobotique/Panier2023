c60572a08c58c1f28d52f33f1209eb8ffab5badd
ENAC-Robotique <robotique.enac@gmail.com>
2023-05-16 15:17:07 +0200

    hardcode number of cerises to 10

--- src/main.cpp
+++ src/main.cpp
@@ -8,24 +8,21 @@
 #define SOUND_PIN 25
 #define COUNT_BUTTON 39
 
-char test[] = "test0";
-int buttonPressed =0;
-uint32_t lastPressedTimeStamp =0;
+int buttonPressed = 0;
+uint32_t lastPressedTimeStamp = 0;
 XT_DAC_Audio_Class dacAudio(SOUND_PIN,0);
 DJukeBox soundPlayer(&dacAudio);
 WD202A display;
-char buffer[14];
-int cherryNumber=-1;
+int cherryNumber = -1;
 
 void gainCherry(void){
   cherryNumber++;
 
   soundPlayer.playSound(1);
-  sprintf(buffer,"Cerises : %d",cherryNumber);
   display.clearDisplay();
 
   display.moveLine(0,1);
-  display.write(buffer, ((cherryNumber >=10) ? 12 : 11));
+  display.write("Cerises : 10", 12);
 }
 void setup() {
   pinMode(25,OUTPUT);
@@ -35,21 +32,18 @@ void setup() {
   pinMode(COUNT_BUTTON,INPUT);
   delay(100);
   gainCherry();
-  // put your setup code here, to run once:
-
 }
 
 
 void loop() {
   soundPlayer.update();
-  if (!digitalRead(COUNT_BUTTON)){
+  if (!digitalRead(COUNT_BUTTON)) {
     lastPressedTimeStamp = millis();
     buttonPressed = 1;
-  }
-  else if(buttonPressed && (millis() - lastPressedTimeStamp)>5){
-    buttonPressed=0;
+  } else if (buttonPressed && (millis() - lastPressedTimeStamp) > 5) {
+    buttonPressed = 0;
     gainCherry();
   }
-  
+ 
 }
 
